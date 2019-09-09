029dec1f64f0cd43c1ddce14046044dfeeab6714
Gaspar <gasparmando@hotmail.com>
2019-09-09 20:08:18 -0300

    Trabajo Practico 1

--- ArmandoG_MantayR/TP1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/workspace/TP1a/Test6SRA_isim_beh.exe" -prj "D:/workspace/TP1a/Test6SRA_beh.prj" "work.Test6SRA" "work.glbl" 
